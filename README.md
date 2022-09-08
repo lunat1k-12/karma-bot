8edc457c1a405fb619359c182b1028d6c9bac87e
yvgenchemeris <cheneris.lunat1k@gmail.com>
2022-09-08 19:42:51 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,21 @@
+# Local Run
+
+### Pre requirements
+Java - AdoptOpenJDK-11 <br/>
+Docker <br/>
+Registered Telegram bot token
+
+### Docker
+
+You need to create a DB with the next command:
+
+`docker run --name chat -p5434:5432 -e POSTGRES_PASSWORD=1234 -e POSTGRES_USER=chat -e POSTGRES_DB=chat -d postgres`
+
+### System variables
+
+System variable with telegram bot token needed:
+`TEST_KARMA_BOT_TOKEN`
+
+### Run command
+
+`./gradlew clean -Dspring.profiles.active=local bootRun`
--- src/main/java/com/coolguys/bot/BotApplication.java
+++ src/main/java/com/coolguys/bot/BotApplication.java
@@ -1,7 +1,5 @@
 package com.coolguys.bot;
 
-import com.coolguys.bot.listner.MessagesListener;
-import com.pengrad.telegrambot.TelegramBot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,11 +15,8 @@ public class BotApplication implements CommandLineRunner {
         SpringApplication.run(BotApplication.class, args);
     }
 
-    private final MessagesListener listener;
-
     @Override
     public void run(String... args) throws Exception {
 
     }
 }
-// docker run --name chat -p5434:5432 -e POSTGRES_PASSWORD=1234 -e POSTGRES_USER=chat -e POSTGRES_DB=chat -d postgres
