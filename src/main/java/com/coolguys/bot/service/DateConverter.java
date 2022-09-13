988ca889c61d740fe824480d41c94f2a26a130a9
yvgenchemeris <cheneris.lunat1k@gmail.com>
2022-09-13 17:38:01 +0300

    update date converter

--- src/main/java/com/coolguys/bot/service/DateConverter.java
+++ src/main/java/com/coolguys/bot/service/DateConverter.java
@@ -2,13 +2,16 @@ package com.coolguys.bot.service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class DateConverter {
 
     public static String localDateTimeToStringLabel(LocalDateTime ldt) {
+        ZonedDateTime zdt = ldt.toInstant(ZoneOffset.UTC).atZone(ZoneId.of("UTC+03:00"));
         return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-                .format(ldt.atZone(ZoneId.of("UTC+03:00")));
+                .format(zdt);
     }
 }
